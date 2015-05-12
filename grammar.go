ac564471652be284f73490756a26afa8a451d5f8
Cheer Xiao <xiaqqaix@gmail.com>
2015-05-12 22:40:45 +0200

    Remove an unused function

--- grammar.go
+++ grammar.go
@@ -33,28 +33,3 @@ func newGrammar(ps []production, ss string) *grammar {
 	}
 	return g
 }
-
-/*
-func parseGrammar(spec string) *grammar {
-	pos := 0
-	g := &grammar{make(map[string]bool), make(map[string]bool), nil, ""}
-
-	read := func() {
-		r, _ := utf8.DecodeRuneInString(spec[pos:])
-	}
-
-	skipWhitespace := func() {
-		for pos < len(spec) {
-			r, _ := utf8.DecodeRuneInString(spec[pos:])
-		}
-	}
-
-	pGrammar := func() {
-	}
-
-	pProduction := func() {
-	}
-
-	return g
-}
-*/
